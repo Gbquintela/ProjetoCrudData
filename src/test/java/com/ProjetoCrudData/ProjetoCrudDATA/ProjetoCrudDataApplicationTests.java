c6314d7fa0eec9335d4e9236dc67af8fe32bcfc5
Gabriel Quintela <gabriel_martinhoq@hotmail.com>
2025-03-19 15:55:21 -0300

    Arquivo teste mas nao utilizei. Proxima etapa dos meus estudos e TESTES em java.

---
+++ src/test/java/com/ProjetoCrudData/ProjetoCrudDATA/ProjetoCrudDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjetoCrudData.ProjetoCrudDATA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCrudDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
