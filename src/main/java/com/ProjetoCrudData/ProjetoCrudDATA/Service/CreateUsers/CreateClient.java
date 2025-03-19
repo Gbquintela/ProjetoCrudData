f0c048c11685af84eee5bf72b39a1c2e6c994d9b
Gabriel Quintela <gabriel_martinhoq@hotmail.com>
2025-03-19 15:44:41 -0300

    Arquivo de criação de Cliente ela possui toda logica para criar e adicionar esse usuario do tipo Client.

---
+++ src/main/java/com/ProjetoCrudData/ProjetoCrudDATA/Service/CreateUsers/CreateClient.java
@@ -0,0 +1,49 @@
+package com.ProjetoCrudData.ProjetoCrudDATA.Service.CreateUsers;
+
+import com.ProjetoCrudData.ProjetoCrudDATA.ORM.User;
+import com.ProjetoCrudData.ProjetoCrudDATA.ORM.UserType;
+import com.ProjetoCrudData.ProjetoCrudDATA.Repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
+
+@Service // Indica que esta classe é um serviço gerenciado pelo Spring
+public class CreateClient {
+
+    private UserRepository userRepository; // Dependência do repositório para salvar usuários no banco
+
+    // Construtor para injetar o UserRepository no serviço
+    public CreateClient(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    // Metodo para criar um cliente no sistema
+    public void createClient(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to menu Register!");
+
+        // Captura os dados do novo cliente via entrada do usuário
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter your CPF: ");
+        String CPF = scanner.nextLine();
+
+        System.out.println("Enter your Email: ");
+        String email = scanner.nextLine();
+
+        System.out.println("Enter your nickname: ");
+        String nickname = scanner.nextLine();
+
+        System.out.println("Enter your password: ");
+        String password = scanner.nextLine();
+
+        // Cria um novo objeto User com tipo Client
+        User users = new User(CPF, UserType.Client, nickname, password, name, email);
+
+        // Salva o usuário no banco de dados
+        this.userRepository.save(users);
+
+        System.out.println("Client created!");
+    }
+}
